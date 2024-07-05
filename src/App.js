de0358c4378ed3197b20265e1eda4026bd84c799
JamesMoabi <jamesmoabi8@gmail.com>
2024-07-05 16:12:03 +0200

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/header"
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import logo from "../images/logo.svg";
+import arrowdown from "../images/icon-arrow-down.svg";
+import arrowup from "../images/icon-arrow-up.svg";
+import calander from "../images/icon-calendar.svg";
+import todolist from "../images/icon-todo.svg";
+import reminder from "../images/icon-reminders.svg";
+import planning from "../images/icon-planning.svg";
+import heroImage from "../images/image-hero-desktop.png";
+import clientDatabiz from "../images/client-databiz.svg";
+import clientAudiophile from "../images/client-audiophile.svg";
+import clientMeet from "../images/client-meet.svg";
+import clientMaker from "../images/client-maker.svg";
+import './header.css';
+
+function Header() {
+  const [showFeaturesList, setShowFeaturesList] = useState(false);
+  const [showCompanyList, setShowCompanyList] = useState(false);
+
+  const toggleFeaturesList = () => {
+    setShowFeaturesList(!showFeaturesList);
+  };
+
+  const toggleCompanyList = () => {
+    setShowCompanyList(!showCompanyList);
+  };
+
+  return (
+    <div className='NavBar'>
+      <div className="nav-container">
+        <div className="nav-left">
+          <img src={logo} className='NavBar-logo' alt='logo'/>
+          <ul className="nav-list">
+            <li onClick={toggleFeaturesList}>
+              Features <img src={showFeaturesList ? arrowup : arrowdown} alt={showFeaturesList ? "Arrow Up" : "Arrow Down"} className="arrow-icon" />
+              {showFeaturesList && (
+                <ul className="sub-menu">
+                  <li><img src={todolist} alt="Todo List"/>Todo List</li>
+                  <li><img src={calander} alt="Calendar"/>Calendar</li>
+                  <li><img src={reminder} alt="Reminders"/>Reminders</li>
+                  <li><img src={planning} alt="Planning"/>Planning</li>
+                </ul>
+              )}
+            </li>
+            <li onClick={toggleCompanyList}>
+              Company <img src={showCompanyList ? arrowup : arrowdown} alt={showCompanyList ? "Arrow Up" : "Arrow Down"} className="arrow-icon" />
+              {showCompanyList && (
+                <ul className="sub-menu">
+                  <li>About Us</li>
+                  <li>Our Team</li>
+                  <li>Our Culture</li>
+                </ul>
+              )}
+            </li>
+            <li>Careers</li>
+            <li>About</li>
+          </ul>
+        </div>
+        <div className="top-right">
+          <li>Login</li>
+          <button className="register-btn">Register</button>
+        </div>
+      </div>
+      <div className="content">
+        <div className="text-content">
+          <h1 className="large-text">Make<br/>remote work</h1>
+          <p>Get your team in sync, no matter your location.<br/>Streamline processes, create team rituals and watch productivity soar.</p>
+          <button className="learn-more-btn">Learn more</button>
+        </div>
+        <img src={heroImage} className='Hero-image' alt='hero'/>
+      </div>
+      <div className='Clients'>
+        <img src={clientDatabiz} alt='Client Databiz'/>
+        <img src={clientAudiophile} alt='Client Audiophile'/>
+        <img src={clientMeet} alt='Client Meet'/>
+        <img src={clientMaker} alt='Client Maker'/>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
